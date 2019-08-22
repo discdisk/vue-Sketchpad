52c733e1d89d0e286a3c2a16589f278c9a937149
discdisk <32610374+discdisk@users.noreply.github.com>
2019-08-22 16:09:13 +0900

    update
    add axios to make XMLHttpRequests and do image recognition on server side

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,25 @@
 <template>
-  <div>
+  <div id="skechPad">
     <h1>{{msg}}</h1>
+    <div>
+      <label style=" ">recognition service URL: </label>
+      <input type="text" v-model=ImageProcessServer placeholder="http://localhost:5000/demotext" style="width: 50%; margin-bottom:5px;">
+    </div>
     <div class="btnFunc">
       <button v-on:click ="clearCanvas">clear</button>
       <button id="ebtn" v-on:click ="eraser">eraser</button>
       <button v-on:click ="send">send</button>
     </div>
-    <input v-on:change='sliderValue' type="range" min="1" max="30" value="10" class="slider" id="myRange">
-    <p>The recognition result is {{ result }} .</p>
-    <canvas id="mycanvas" v-on:mousedown="startDraw" v-on:mousemove="draw" v-on:mouseup="stopDraw" @touchstart.prevent="startDraw" @touchmove.prevent="drawTouch" width="400" height="400" style=" border:6px solid #c3c3c3;">not supported!</canvas>
+    <input v-model='paintSize' type="range" min="1" max="30" value=10 class="slider" id="myRange">
+    <h2 v-if='recognitionSuccess'>The recognition result is {{ result }} .</h2>
+    <h2 v-else>ERROR.</h2>
+    <canvas id="mycanvas" v-on:mousedown="startDraw" v-on:mousemove="draw" v-on:mouseup="stopDraw" v-on:mouseleave="stopDraw" @touchstart.prevent="startDraw" @touchmove.prevent="drawTouch" width="400" height="400" style="background:green; border:1px solid #c3c3c3; margin:0 auto;">not supported!</canvas>
+    <div style="height:100px"></div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   name: 'HelloWorld',
   props: {
@@ -20,17 +27,19 @@ export default {
   },
   data: function(){
     return {
+      recognitionSuccess:true,
       result:'_',
+      ImageProcessServer:"http://localhost:5000/demotext",
       isCanvasMouseDown:false,
       paintSize:10,
       usePen:true,
-      paintColor:"#ccccff",
-      penColor:"#ccccff",
+      paintColor:"black",
+      penColor:"black",
       eraserColor:"white"}
   },
   methods:{
     log(e){
-      console.log(e.changedTouches[0])
+      console.log(e)
     },
     drawTouch(e){
       if(this.isCanvasMouseDown){
@@ -43,8 +52,15 @@ export default {
       }
     },
     send(){
-      var canvas = document.getElementById("mycanvas");
-      console.log(canvas.toDataURL())
+      console.log(this.canvas.toDataURL())
+      this.result='...'
+      // fetch('http://localhost:5000/demotxt',{imgURI:this.canvas.toDataURL()})
+      axios({
+        method: 'post',
+        url: this.ImageProcessServer,
+        data: {imgURI:this.canvas.toDataURL()}
+      }).then(response => (this.result = response["data"])).then(this.recognitionSuccess=true).catch(error => (this.recognitionSuccess=false))
+
       
     },
     eraser(){
@@ -58,9 +74,6 @@ export default {
         eraserBtn.style.background = "oldlace";
       }
     },
-    sliderValue(e){
-      this.paintSize=e.target.value
-    },
     startDraw(){
       this.isCanvasMouseDown=true;
     },
@@ -69,46 +82,55 @@ export default {
     },
     draw(e){
       if(this.isCanvasMouseDown){
-        var canvas = document.getElementById("mycanvas");
-        var ctx = canvas.getContext('2d');
-        ctx.beginPath();
-        ctx.fillStyle = this.paintColor;
-        ctx.arc(e.layerX,e.layerY,this.paintSize,0,2*Math.PI);  //arc(x,y,r,start,stop)
-        ctx.fill();
+        this.ctx.beginPath();
+        this.ctx.fillStyle = this.paintColor;
+        this.ctx.arc(e.layerX,e.layerY,this.paintSize,0,2*Math.PI);  //arc(x,y,r,start,stop)
+        this.ctx.fill();
       }
     },
     clearCanvas(){
-      var canvas = document.getElementById("mycanvas");
-      var myRange = document.getElementById("myRange");
-      console.log(myRange)
-      var ctx = canvas.getContext('2d');
-      
-      ctx.clearRect(0,0,canvas.width,canvas.height);  //(x,y,width,height)
+      this.ctx.fillStyle = "white";
+      this.ctx.rect(0,0,this.canvas.width,this.canvas.height);  //(x,y,width,height)
+      this.ctx.fill();
     }
 
+  },
+  mounted () {
+    this.canvas = document.getElementById("mycanvas");
+    this.ctx = this.canvas.getContext('2d');
+    this.clearCanvas()
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#skechPad{
+  margin:0 auto;
+  width:400px;
+}
+
 .btnFunc{
-  disply:flex;
+  display: flex;
+  display: -webkit-flex;
 }
 
 button {
   border:hidden;
+  font-size: 30px;
   flex:1;
+  -webkit-box-flex: 1;
   margin: 1px;
-  width: 17%;
   background:palegreen;
 
 }
 
-
 .slider {
+  
+  margin: 1px;
+  margin-top:10px;
   -webkit-appearance: none;
-  width: 50%;
+  width: 100%;
   height: 20px;
   background: #d3d3d3;
   outline: none;
